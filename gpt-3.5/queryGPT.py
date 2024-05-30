09e5f8dda880e46f12111efe3cce1eab771b6659
CS-matthew-maisonave <matthewmaisonave@gmail.com>
2024-05-29 21:21:25 -0400

    Update queryGPT.py

--- gpt-3.5/queryGPT.py
+++ gpt-3.5/queryGPT.py
@@ -1,10 +1,13 @@
-import openai, time
+import time, os, openai
 import pandas as pd
 
-# Key removed, replace with valid key
-openai.api_key = "input API key here"
+#from openai import OpenAI
+openai.api_key = ' '
 
-df = pd.read_csv('input.csv')
+# Key removed, replace with valid key
+file_path = 'gpt-3.5/data2/claim_level_groundtruth.csv'
+
+df = pd.read_csv(file_path)
 
 claimIDs = []
 claims = []
@@ -14,11 +17,11 @@ responses = []
 requests = 0
 
 # Adjust temperature
-temp = 0.75
+temp = 0.0
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
+    response = openai.chat.completions.create(
         model=model,
         messages=messages,
         temperature=temp, # This is the degree of randomness of the model's output
@@ -29,29 +32,29 @@ for index, row in df.iterrows():
     claimIDs.append(row['id'])
     claims.append(row['claim'])
     abstracts.append(row['published_paper_abstract'])
-    
+
 print("Done with CSV read")
-    
+
 for index, row in df.iterrows():
-     
+
     prompt = "Claim: " + claims[index] +"\nAbstract: " + abstracts[index] + "\n Question: Is the abstract relevant to the claim? Answer with one word and a number: SUPPORT if the abstract supports the claim, CONTRADICT if the abstract contradicts the claim or NEI if the abstract does not provide enough information about the claim to decide along with a number on a scale of 0-1000 rate how relevant the abstract is to the claim.  \n Answer: "
-    
+
     if requests>0 and requests%20 == 0:
         print("Sleeping for 70 seconds...") # Can only make 20 calls a minute, sleep time can be lower but this worked for me
         time.sleep(70)
         print("Continuing...currently on request " + str(requests))
-        
+
     requests += 1
-    
+
     responses.append(get_completion(prompt))
-    
-    df.at[index, 'GPT_Response_0.75'] = responses[index] # Name of column in output file
-    
+
+    df.at[index, 'GPT_Response_0.0'] = responses[index] # Name of column in output file
+
 # Export to CSV
-df.to_csv('exported3.csv')
+df.to_csv('../data2/000_temp_consensus.csv')
 
 print("Done with export")
 
-    
-    
+
+
 
--- gpt-3.5/queryGPTRationale.py
+++ gpt-3.5/queryGPTRationale.py
@@ -2,7 +2,7 @@ import openai, time
 import pandas as pd
 
 inFile = 'indexed_rationale_annotations.csv'
-outFile = 'indexed_rationale_annotations_query_responses_075.csv'
+outFile = 'indexed_rationale_annotations_query_responses_0.0.csv'
 
 # Insert valid API key
 openai.api_key = "sk..."
@@ -10,7 +10,7 @@ openai.api_key = "sk..."
 requests = 0
 
 # Adjust GPT temperature
-temp = 0.75
+temp = 0.0
 i = 0
 claimIDs = [] 
 abstracts = []
@@ -32,65 +32,64 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 df = pd.read_csv(inFile)
 
 for index, row in df.iterrows():
-    
+
     # Used to compare next id in index during iteration
     claimIDs.append(row['id'])
-    
+
 for index, row in df.iterrows():
-    
+
     currentID = row['id']
-    
+
     if(index <= (len(claimIDs) - 2)):
         nextID = claimIDs[index + 1]
-        
+
     if(index == (len(claimIDs) - 1)):
         nextID = 0
-    
+
     abstracts.append(row['published_paper_abstract'] + "\n")
     abstractsNoBrackets = ''.join(abstracts)
 
     i += 1
-    
+
     if(nextID != currentID):
         prompts.append("Claim: " + str(row['claim']) + "\n" + question + abstractsNoBrackets)
         abstracts = []
         abstractsNoBrackets = []
         i = 0
-        
+
 promptsDF = pd.DataFrame(prompts, columns=['prompts'])
 promptsDF = pd.concat([df, promptsDF], axis=1)
 
 print('Done with prompt generation starting query\n')
 
 for pos, p in enumerate(prompts):
-    
+
     if requests>0 and requests%20 == 0:
-        
+
         # Can make 20 calls a minute, sleep time can be lower but this worked for me
         print("\nSleeping for 70 seconds...\n")
         time.sleep(70)
         print("Continuing...currently on request " + str(requests))
-        
+
     attempts = 0
     success = False
-    
+
     # Kept getting ServiceUnavailableError without try block
     while attempts < 10 and not success:
-    
+
         try:
             responses.append(get_completion(p))
             success = True
-        
-        except openai.error.ServiceUnavailableError as e:
+
+        except openai.ServiceUnavailableError as e:
             attempts += 1
             if attempts == 10:
                 break
-        
+
     # Name of column in output file
-    promptsDF.at[pos, 'GPT_Response_0.75'] = responses[pos] 
-    
+    promptsDF.at[pos, 'GPT_Response_0.0'] = responses[pos] 
+
     requests += 1
-    
+
 promptsDF.to_csv(outFile, index=False)
 print('\nDone with export!')
-    
